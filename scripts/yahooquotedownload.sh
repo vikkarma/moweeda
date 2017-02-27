1b395019357797110ba2971fe4251f3841a51658
vvishwakarma <vvishwakarma@salesforce.com>
2017-02-27 08:45:23 +0530

    working copy of yahoo quote downloader

--- scripts/yahooquotedownload.sh
+++ scripts/yahooquotedownload.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
+
+###############################################
+# change date in YAHOO_QUOTE_URL_PARTA to run #
+# FILE=../config/yahoofno
+# FILE=../config/yahoomidcal150
+###############################################
+
 FILE=$1
-#FILE=../config/yahoofno
-#FILE=../config/yahoomidcal150
+BASEDIR=$(dirname $0)
+if [ ! -z "$BASEDIR" ]; then
+    echo "changing current dir to $BASEDIR"
+    cd $BASEDIR
+fi
+
 YAHOO_QUOTE_URL_PARTA='http://real-chart.finance.yahoo.com/table.csv?a=02&b=1&c=2016&d=01&e=25&f=2017&g=d&ignore=.csv&s='
 while read line; do
-     echo "getting istorical quote for : $line"
-     chomp($line)
-     YAHOO_QUOTE_URL="${YAHOO_QUOTE_URL_PARTA}$line" #outputs table.csv
-     wget $YAHOO_QUOTE_URL
+     echo "getting historical quote for : $line"
+     #chomp $line
+     YAHOO_QUOTE_URL="${YAHOO_QUOTE_URL_PARTA}$line" 
+     # get quote file
      QUOTE_FILE=${line}.csv
-     mv table.csv $QUOTE_FILE 
-     cat $QUOTE_FILE | awk -F, '{print $(NF-2)}' > close_${QUOTE_FILE} 
-     cat $QUOTE_FILE | awk -F, '{print $(NF-1)}' > vol_${QUOTE_FILE}
+     curl -s $YAHOO_QUOTE_URL > "../tmp/$QUOTE_FILE"
+     # get close file
+     cat ../tmp/$QUOTE_FILE | awk -F, '{print $(NF-2)}' > ../stockdata/close_${QUOTE_FILE} 
+     sed '/Close/d' ../stockdata/close_${QUOTE_FILE}
+     # get volume file
+     cat ../tmp/$QUOTE_FILE | awk -F, '{print $(NF-1)}' > ../stockdata/vol_${QUOTE_FILE}
+     sed '/Volume/d' ../stockdata/vol_${QUOTE_FILE}
+     sleep 3
 done < $FILE
+
+######### Once the quote files are generated run_moweeda.sh on stock data .. enjoy
